a194db908de8fcee392d4b5d5a811e52241b9f7e
wz-fu <69373512+wz-fu@users.noreply.github.com>
2023-03-18 17:58:42 +0800

    Update login.py

--- src/login.py
+++ src/login.py
@@ -1,1 +1,44 @@
+'''
+Author: fwz
+e-mail: wz-fu@qq.com
+Date: 2023-03-18 16:38:08
+LastEditTime: 2023-03-18 17:55:14
+Description: 自动签到gptdos
+'''
+import requests
+import json
+import sys
+
+
+def login():
+    url = "https://gptdos.com/api/user/info"
+    refer = "https://gptdos.com/chat"
+    cookie = sys.argv[1]
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+    requests.get(url=refer,
+                 headers={
+                     'cookie': cookie,
+                     'refer': refer,
+                     'user-agent': user_agent
+                 })
+
+    response = requests.get(url=url,
+                            headers={
+                                'cookie': cookie,
+                                'refer': refer,
+                                'user-agent': user_agent
+                            })
+    jsonData = json.loads(response.text)
+    try:
+        msg = jsonData['data']['userInfo']['tokens_available']
+    except Exception:
+        msg = 'cookie失效'
+
+    key = sys.argv[2]
+    requests.get(url='https://api2.http://www.pushplus.plus/send?token=' +
+                 key + msg)
+
+
+if __name__ == "__main__":
+    login()
 
