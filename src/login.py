fa10615242d02ef8264775a149b4f2736ac1c8d2
incoparab <69373512+incoparab@users.noreply.github.com>
2023-03-27 16:29:10 +0800

    Update login.py

--- src/login.py
+++ src/login.py
@@ -2,7 +2,7 @@
 Author: fwz
 e-mail: wz-fu@qq.com
 Date: 2023-03-18 16:38:08
-LastEditTime: 2023-03-20 11:35:31
+LastEditTime: 2023-03-27 26:28:31
 Description: 自动签到gptdos
 '''
 import requests
@@ -14,7 +14,6 @@ def login():
     url = "https://gptdos.com/api/user/info"
     refer = "https://gptdos.com/chat"
     cookie = sys.argv[1]
-    # print(cookie)
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
     requests.get(url=refer,
                  headers={
@@ -36,12 +35,10 @@ def login():
         msg = 'cookie失效'
 
     key = sys.argv[2]
-    # print(key)
-    sendurl = 'http://www.pushplus.plus/send?token=' + key + '&title=GPTDOS消息' + '&content=' + str(
-        msg)
-    # print(sendurl)
+    title = 'GPTDOS消息'
+    msg = str(msg)
+    sendurl = f'http://www.pushplus.plus/send?token={key}&title={title}&content={msg}'
     requests.get(url=sendurl)
-    # print(msg)
 
 
 if __name__ == "__main__":
